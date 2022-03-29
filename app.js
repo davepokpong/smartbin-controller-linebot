3a76053d2bdbde5075b8c6434d203a383c88492b
Pokpong Noppakhun <dave@Pokpongs-MacBook-Pro.local>
2022-03-29 17:34:51 +0700

    update

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const port = process.env.PORT || 4000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.post('/webhook', async (req, res) => {
+ 
+
+app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     let msg = req.body.events[0].message.text
     let replymsg
